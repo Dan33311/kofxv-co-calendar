8e1140a3f59071decd1de1d8a183c48ecd04b85b
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2022-03-09 13:23:25 -0500

    add styles and transform data to json

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,39 +1,21 @@
-import predatorLogo from '/images/Predator.png'
-import bigGamesLogo from '/images/big-games.png'
+import tournaments from '../../data/tournaments.json'
 
-const tournaments = [
-  {
-    name: 'predator league',
-    img: predatorLogo,
-    date: '19 de marzo',
-    method: 'online',
-    plataform1: 'pc',
-    plataform2: 'playstation'
-  },
-  {
-    name: 'big game',
-    img: bigGamesLogo,
-    date: '02 de abril',
-    method: 'presencial',
-    plataform1: 'pc',
-    plataform2: ''
-  },
-]
 
 const TournamentCards = () => {
   return (
     <div>
       {tournaments.map((tournament, index) => (
-        <div className='cards' key={index}>
-          <h2>{tournament.name.toUpperCase()}</h2>
-          <img src={tournament.img} alt="" />
+        <div className='card' key={index}>
           <p className='date'>{tournament.date.toUpperCase()}</p>
+          <img src={`/images/${tournament.img}.png`} alt="" />
+          <h2>{tournament.name.toUpperCase()}</h2>
           <div className='type'>
-            {tournament.method === 'online' ? <p className='p-online'>{tournament.method.toUpperCase()}</p> : <p className='p-in-person'>{tournament.method.toUpperCase()}</p>}
+            {tournament.method === 'online' ? <p className='method p-online'>{tournament.method.toUpperCase()}</p> : <p className='method p-in-person'>{tournament.method.toUpperCase()}</p>}
             <p className='plataform'>{tournament.plataform1.toUpperCase()}</p>
             {tournament.plataform2 === '' ? null : <p className='plataform'>{tournament.plataform2.toUpperCase()}</p>}
           </div>
-      </div>
+          {tournament.method === 'online' ? <div className='method-line l-online'></div> : <div className='method-line l-in-person'></div>}
+        </div>
       ))}
     </div>
   );
