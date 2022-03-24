4e266f08f76054625583f93032444b515e204ac6
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2022-03-23 23:30:37 -0500

    add main view

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import './App.css'
-import logo from '/images/kofcolombia-logo.png'
+import logo from '/images/kofcolombia-logo-2.png'
 import Cards from './components/Cards'
 
 function App() {
 
   return (
     <div className="App">
-      <div className="navbar">
-        <img className='nav-logo' src={logo} alt="logo" />
-        {/* <h1>TORNEOS</h1> */}
+      <div className="header">
+        <img src='/images/kofxv-logo.png' alt="logo" />  
+        <h1>proximos eventos:</h1>
       </div>
       <Cards />
+      <div className="navbar">
+        <img className='nav-logo' src={logo} alt="logo" />
+        <p>Bogota - Colombia</p>
+        <p>2022 - Daniel Sandoval</p>
+      </div>
     </div>
   )
 }
